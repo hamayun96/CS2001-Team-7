b7b9667d19eb66423fe29d26326fdcc866718dc5
MoBushra <mbushra07@gmail.com>
2017-02-20 16:47:58 +0000

    User can now reset password

--- Tunel/app/src/main/java/uk/ac/brunel/tunel/activity/ResetPassActivity.java
+++ Tunel/app/src/main/java/uk/ac/brunel/tunel/activity/ResetPassActivity.java
@@ -1,12 +1,13 @@
 /*
- * Created by Mohamed Bushra on 20/02/17 16:44
+ * Created by Mohamed Bushra on 20/02/17 16:47
  * Copyright (c) 2017. All rights reserved.
  *
- * Last Modified 20/02/17 16:44.
+ * Last Modified 20/02/17 16:47.
  */
 
 package uk.ac.brunel.tunel.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -56,17 +57,24 @@ public class ResetPassActivity extends AppCompatActivity implements View.OnClick
         }
 
         progressBar.setVisibility(View.VISIBLE);
-        authRef.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
-                @Override
-                public void onComplete(@NonNull Task<Void> task) {
-                if (task.isSuccessful()) {
-                    Toast.makeText(ResetPassActivity.this, "We have sent you instructions to reset your password!", Toast.LENGTH_SHORT).show();
-                } else
-                {
-                    Toast.makeText(ResetPassActivity.this, "Failed to send reset email!", Toast.LENGTH_SHORT).show();
-                }
+        authRef.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>()
+        {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+            if (task.isSuccessful()) {
+                Toast.makeText(ResetPassActivity.this, "We have sent you instructions to reset" +
+                        " your password!", Toast.LENGTH_SHORT).show();
+                // Launch login activity
+                Intent loginIntent = new Intent(ResetPassActivity.this, SignInActivity.class);
+                startActivity(loginIntent);
+                finish();
+            } else
+            {
+                Toast.makeText(ResetPassActivity.this, "Failed to send reset email!",
+                        Toast.LENGTH_SHORT).show();
+            }
 
-                progressBar.setVisibility(View.GONE);
+            progressBar.setVisibility(View.GONE);
             }
         });
     }
